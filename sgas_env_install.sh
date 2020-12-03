9cfff65a123a080cb4b302f26420a1c5aae9c96f
Guocheng Qian <guocheng.qian@outlook.com>
2020-12-03 13:04:26 +0300

    PyG changes the command for installation

--- sgas_env_install.sh
+++ sgas_env_install.sh
@@ -23,11 +23,12 @@ conda install -y pytorch=1.4.0 torchvision cudatoolkit=10.0 python=3.6.8 Pillow=
 pip install tqdm tensorboardX graphviz
 
 # install pyg
+#  https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
 CUDA=cu100
-pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-pip install torch-cluster==1.4.5
+pip install torch-scatter==2.0.4 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-sparse==0.6.1 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-spline-conv==1.2.0 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-cluster==1.4.5 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
 pip install torch-geometric==1.4.3
+pip install requests
 
-conda activate sgas
